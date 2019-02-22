cc9f1e77422e4deda034513ae86fa2211c8d90cc
Javier Gamarra <nhpatt@gmail.com>
2019-02-22 01:33:00 +0100

    _ :P

--- src/app.js
+++ src/app.js
@@ -13,9 +13,9 @@ const ADMIN_USER = process.env.ADMIN_USER || 'admin';
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || 'admin';
 
 const config = {
-    title: 'BilboStack',
+    title: 'WeCode',
     year: '2019',
-    twitter: '@bilbostack',
+    twitter: '@wecodefest',
     baseUrl: process.env.BASE_URL || 'http://localhost:8000'
 };
 
