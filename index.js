90e914bfe21beb77ea175a2eebe6935f9e516e43
Javier Gamarra <nhpatt@gmail.com>
2019-03-03 20:47:02 +0100

    https

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cluster = require('cluster');
 const debug = process.env.NODE_ENV !== 'production';
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 80;
 
 function getNumWorkers() {
     return require('os').cpus().length;
@@ -28,5 +28,38 @@ if (cluster.isMaster && !debug) {
 } else {
     const talks = require('./src/data/talks');
     const app = require('./src/app')(talks);
-    app.listen(PORT, '0.0.0.0');
+	//    app.listen(PORT, '0.0.0.0');
+
+
+
+	const fs = require('fs');
+const http = require('http');
+const https = require('https');
+
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/feedback.wecodefest.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/feedback.wecodefest.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/feedback.wecodefest.com/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+app.use((req, res) => {
+	res.send('Hello there !');
+});
+
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
 }
