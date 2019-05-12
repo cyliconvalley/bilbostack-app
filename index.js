0d622b42c11c2621ce403133a96e478da42d78a9
Javier Gamarra <nhpatt@gmail.com>
2019-05-12 16:16:48 +0200

    bad cert path

--- index.js
+++ index.js
@@ -39,9 +39,9 @@ if (cluster.isMaster && !debug) {
     // Certificate
     if (ssl) {
 
-        const privateKey = fs.readFileSync('/etc/letsencrypt/live/feedback.wecodefest.com/privkey.pem', 'utf8');
-        const certificate = fs.readFileSync('/etc/letsencrypt/live/feedback.wecodefest.com/cert.pem', 'utf8');
-        const ca = fs.readFileSync('/etc/letsencrypt/live/feedback.wecodefest.com/chain.pem', 'utf8');
+        const privateKey = fs.readFileSync('/etc/letsencrypt/live/feedback.lechazoconf.com/privkey.pem', 'utf8');
+        const certificate = fs.readFileSync('/etc/letsencrypt/live/feedback.lechazoconf.com/cert.pem', 'utf8');
+        const ca = fs.readFileSync('/etc/letsencrypt/live/feedback.lechazoconf.com/chain.pem', 'utf8');
 
         const credentials = {
             key: privateKey,
