2fa9ab8c49d1a5c998d5e3ba383177d455e30caa
root <root@Oscar-PC.localdomain>
2020-01-22 21:55:13 +0100

    Compose

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ if (cluster.isMaster && !debug) {
 } else {
     const talks = require('./src/data/talks');
     const app = require('./src/app')(talks);
-    app.listen(PORT, '0.0.0.0');
+    //app.listen(PORT, '0.0.0.0');
 
     console.log('HTTP Server running on port 80');
 
@@ -55,9 +55,13 @@ if (cluster.isMaster && !debug) {
     const httpServer = http.createServer(app);
     // const httpsServer = https.createServer(credentials, app);
 
-    // httpServer.listen(80, () => {
-    //     console.log('HTTP Server running on port 80');
-    // });
+    httpServer.listen(80, () => {
+        console.log('HTTP Server running on port 80');
+    });
+
+    httpServer.listen(443, () => {
+        console.log('HTTP Server running on port 80');
+    });
 
     // httpsServer.listen(443, () => {
     //     console.log('HTTPS Server running on port 443');
